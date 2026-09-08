d427e12d4928e93c38d9daa5775077a45da3f303
amalmicros <amal.micros@atomicmail.io>
2026-09-08 12:41:16 +0500

    Add files via upload

---
+++ script4.py
@@ -0,0 +1,17 @@
+def print_info(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+for item in data_list:
+     if (item['price'] == kwargs.get('price')
+            and item['staff'] == kwargs.get('staff')):
+        filtered_list.append(item)
+        return filtered_list
+
+elif kwargs.get('multiply'):
+result = 1
+for num in args:
+    result *= num
+return result
+
+ПОНЯЛ ТОЛЬКО 2 ЗАДАНИЯ И ЧТО ТО КВАРГС НЕ РАБОТАЕТ типо нет в комплекте
